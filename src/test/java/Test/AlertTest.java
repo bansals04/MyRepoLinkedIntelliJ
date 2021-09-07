1ff1faa2ea8db2b8826fb646f74e6b3ef36483cc
shubhi bansal <shubhi.bansal@globallogic.com>
2021-09-07 20:33:11 +0530

    added method in Alert Test File

--- src/test/java/Test/AlertTest.java
+++ src/test/java/Test/AlertTest.java
@@ -6,18 +6,48 @@ import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
+import java.util.Set;
+
 public class AlertTest extends BaseClass {
 
 
 
     @Test
-    public void openPdfPath()  {
+    public void testAlerts() throws InterruptedException {
         driver.get("http://www.seleniumframework.com/Practiceform/");
         driver.findElement(By.id("alert")).click();
 
         String text = driver.switchTo().alert().getText();
+        Thread.sleep(900);
         driver.switchTo().alert().dismiss();
+
         System.out.println(text);
+        String curHandle=driver.getWindowHandle();
+        System.out.println(curHandle + "____Main window");
+
+        driver.findElement(By.xpath("//button[text()='New Message Window']")).click();
+        Set<String> handles= driver.getWindowHandles();
+        System.out.println(handles.size());
+        for (String handle: handles){
+            System.out.println(handle);
+            if(!curHandle.equals(handle)){
+                driver.switchTo().window(handle);
+
+                System.out.println(driver.getWindowHandle() + "__I am opened dialog window");
+                Thread.sleep(9000);
+                driver.close();
+                driver.switchTo().window(curHandle);
+            }
+        }
+        System.out.println("I have closed the dialog");
+        System.out.println(driver.getCurrentUrl() + "  __current url");
+        System.out.println(driver.getTitle()+ "  __current Title");
+        System.out.println(driver.getWindowHandle() + "  __current Window Handle");
+
+        driver.findElement(By.xpath("//textarea[@name='message']")).sendKeys("Hello world how are you this morning");
+
+
+
 
     }
 
